bcdeab1004901341fc19fa6250789f73b663cc87
Sparsh1502 <113872681+Sparsh1502@users.noreply.github.com>
2023-08-04 16:25:47 +0530

    Here, 3rd test passes.

--- SapceCraftControl.test.js
+++ SapceCraftControl.test.js
@@ -1,4 +1,4 @@
-const {SpaceCraftControl,DirectionIndex} = require('./SpaceCraftControl');
+const {SpaceCraftControl,DirectionIndex,moveForwardOrBackward} = require('./SpaceCraftControl');
 
 describe('Test for only function SpaceCraftControl',()=>{
     it('returns array with  empty commands array',()=>{
--- SpaceCraftControl.js
+++ SpaceCraftControl.js
@@ -1,7 +1,11 @@
 const directions = ['N','E','S','W','Up','Down'];
+let currentDirection;
+let x,y,z;
+
 
 function SpaceCraftControl(initialPosition, initialDirection,commands ) {
-    let [x,y,z] = initialPosition;
+    let [X,Y,Z] = initialPosition;
+    x=X; y=Y;z=Z;
     currentDirection = initialDirection;
     return [x,y,z,currentDirection];
 }
@@ -11,8 +15,37 @@ function DirectionIndex(direction){
     return directions.indexOf(direction)
 }
 
+function moveForwardOrBackward(step){
+    switch (currentDirection){
+        case 'N':
+            y+=step
+            break
+        
+        case 'S':
+             y-=step
+            break
+            
+        case 'E':
+            x+=step
+            break
+
+        case 'W':
+            x-=step
+            break
+
+        case 'Up':
+            z+=step
+            break
+
+        case 'Down':
+            z-=step
+            break
+        
+        default:
+        break
+    }
+}
 
 
-
-module.exports = {SpaceCraftControl,DirectionIndex};
+module.exports = {SpaceCraftControl,DirectionIndex,moveForwardOrBackward};
 
