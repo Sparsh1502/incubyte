6800529c0e99688e5bb74f7c93415dc40edda010
Sparsh1502 <113872681+Sparsh1502@users.noreply.github.com>
2023-08-04 16:11:26 +0530

    Here, 1st testcase passes.

---
+++ SpaceCraftControl.js
@@ -0,0 +1,7 @@
+function SpaceCraftControl(initialPosition, initialDirection,commands ) {
+    let [x,y,z] = initialPosition;
+    currentDirection = initialDirection;
+    return [x,y,z,currentDirection];
+}
+
+module.exports = SpaceCraftControl;
