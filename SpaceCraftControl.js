5169d4f4443b18fb97b96f7f7e10cda3f373e97f
Sparsh1502 <113872681+Sparsh1502@users.noreply.github.com>
2023-08-04 16:36:13 +0530

    Here..6th failed..so adding remaining code..now it passes.

--- SpaceCraftControl.js
+++ SpaceCraftControl.js
@@ -7,6 +7,35 @@ function SpaceCraftControl(initialPosition, initialDirection,commands ) {
     let [X,Y,Z] = initialPosition;
     x=X; y=Y;z=Z;
     currentDirection = initialDirection;
+    for(let command of commands){
+        switch(command){
+            case 'f':
+               moveForwardOrBackward(1);
+               break;  
+
+            case 'b':
+                moveForwardOrBackward(-1)
+                break;
+            
+            case 'l':
+                 turnLeftOrRight(-1)
+                 break
+            
+            case 'r':
+                turnLeftOrRight(1)
+                break
+
+            case 'u':
+            turnUpOrDown(1)
+            break
+
+            case 'd':
+            turnUpOrDown(-1)
+            break
+
+            default:
+                break
+        }
     return [x,y,z,currentDirection];
 }
 
