77528600561823e4baaff843747811b6f0faf062
Sparsh1502 <113872681+Sparsh1502@users.noreply.github.com>
2023-08-04 15:59:40 +0530

    1st testcase falis, as no code.

--- SapceCraftControl.test.js
+++ SapceCraftControl.test.js
@@ -1,7 +1,7 @@
 const SpaceCraftControl = require('./SpaceCraftControl');
 
-describe('main function',()=>{
-    it('empty',()=>{
+describe('Test for only function SpaceCraftControl',()=>{
+    it('returns array with no empty commands array',()=>{
         expect(SpaceCraftControl([0,0,0],'Up',[])).toEqual([0,0,0,'Up']);
     });
     });
